b508a0b68c9f710a62a36436d6b8aa683838c6dc
mdakash648 <akashnazmul648@gmail.com>
2024-06-03 21:58:05 +0600

    simple vue project

---
+++ vueproject/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+
+    <main>
+      <Home />
+    </main>
+  </div>
+</template>
+
+<script>
+import Home from './components/Home.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Home
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vueproject/src/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+    <section id="home">
+        <h1>Student Name : Md Akash</h1>
+        <h2>Student Roll : CMT2122000014</h2>
+        <h2>Student Board Roll : 655245</h2>
+        <h2>semester : 5th</h2>
+
+    </section>
+</template>
+
+<script>
+    export default {
+        name: 'Home'
+    }
+</script>
+
+<style scoped>
+    #home {
+        margin: 20px;
+    }
+    h1,
+    h2 {
+        margin-bottom: 15px;
+    }
+</style>
